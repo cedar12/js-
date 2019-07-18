f274aa3f71d7620dc254acbf7bdbdcebc74ecff1
cedar12 <44419143+cedar12@users.noreply.github.com>
2019-07-18 17:43:04 +0800

    task.js
    js 定时任务

---
+++ task.js
@@ -0,0 +1,6 @@
+/**
+ * js 定时任务
+ * @author cedar12
+ * @date 2019-07-18
+ */
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('q 9(t){5(t&&P t==\'O\'){2 a=t.f().R(\' \');Q(2 i=0;i<a.b;i++){2 e=a[i];5(e.f().b>0){4.e.K(e.f())}}}}9.J={N:9,d:L,e:[],S(){4.d=Y},X(){l\'10：\\n\\Z U=k 9(\\\'y=T M=7 d=18 h=17 m=W s=V\\\');\\n\\I.c(q(){z.A(\\\'v\\\')});\\H\\E:B(C,D-????)\\G:F(1-12)\\r:u(1-x)\\11:1p(1-7,1k)\\1m:1q(0-1i)\\1l:1n(0-j)\\1j:1r(0-j)\'},c(8){5(4.d){l}2 3=k 1o();2 y=3.16();2 M=o(3.19())+1;2 d=3.1a();2 w=o(3.14())+1;2 h=3.15();2 m=3.1b();2 s=3.1f();5(4.e.b>0){2 6=4.e.1g(\'&&\');6=6.1h(/=/g,\'==\');5(1c(6)){8(3)}}1d{8(3)}2 p=4;1e(()=>{p.c(8)},13)}}',62,90,'||let|date|this|if|exp||callback|Task|exps|length|update|||trim||||59|new|return|||parseInt|self|function|nd|||日|开始执行||31||console|log|完整的年份|4位|1970|ny|当前月份|nM|n空格连接多个条件|tdemo|prototype|push|false||constructor|string|typeof|for|split|destory|2019|demo|00|30|help|true|tconst|使用实例|nw||1000|getDay|getHours|getFullYear|||getMonth|getDate|getMinutes|eval|else|setTimeout|getSeconds|join|replace|23|ns|7代表星期天|nm|nh|分钟数|Date|星期几|小时数|秒数'.split('|'),0,{}))
